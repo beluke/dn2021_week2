c985a80376358171224b46ad329a8a6dfe697b2e
Luke Belinc <belinc@tuta.io>
2021-11-19 19:19:25 -0500

    added loops, input validation, menus, and appropriate output

--- StudentDatabase/Program.cs
+++ StudentDatabase/Program.cs
@@ -6,7 +6,90 @@ namespace StudentDatabase
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // 2d student data array
+            // [student_name, hometown, favorite_food]
+            string[,] studentData = {
+                { "John Doe", "Gibraltar, MI", "Apples" },
+                { "Caleb Johnson","Detroi, MI","Pizza" },
+                { "Sara Silverman","Sagniaw, MI","Tomatoes" },
+                { "Kim Petras","Grand Rapids, MI","Gluten Free Bananas" },
+                { "Beyonce Knowles","San Francisco, CA","Bread" },
+                { "Charli XCX","Los Angeles, CA","Cotton Candy" }
+            };
+
+            do
+            {
+                // index variable
+                int idx;
+                try
+                {
+                    // prompt user
+                    Console.Write("Please enter the student number (1-6): ");
+                    idx = int.Parse(Console.ReadLine());
+
+                    // throw new exception if input is out of range
+                    if (idx <= 0 || idx > 6) throw new Exception($"{idx} is out of range. Please try again!");
+                }
+                // handle any exception
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    continue;
+                }
+
+                // output student name
+                Console.WriteLine($"\n\tStudent name: {studentData[idx-1,0]}");
+
+                do
+                {
+                    string category;
+                    // enter try catch for getting category name
+                    try
+                    {
+                        Console.Write("\nPlease enter category (either \"hometown\" or \"favorite food\"): ");
+                        category = Console.ReadLine().ToLower();
+
+                        // throw new exception if category is invalid
+                        if (!category.Equals("favorite food") && !category.Equals("hometown"))
+                            throw new Exception($"Invalid category: {category}. Please try again!");
+                    }
+                    // handle any exception
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                        continue;
+                    }
+
+                    // dispaly info based on category
+                    switch (category) {
+                        case "hometown":
+                            Console.WriteLine($"\n\tHometown: {studentData[idx - 1, 1]}");
+                            break;
+                        case "favorite food":
+                            Console.WriteLine($"\n\tFavorite food: {studentData[idx - 1,2]}");
+                            break;
+                        default:
+                            // should never be reached
+                            Console.WriteLine("\n\tDefault case reached in second menu...");
+                            break;
+                    };
+
+                    // default is break loop
+                    break;
+
+                } while (true);
+
+                Console.Write("\nWould you like to inquire about another stduent? (y/n) (default = n): ");
+                string input = Console.ReadLine().ToLower();
+
+                // only continue if input is not y or yes
+                if (input != "y" && input != "yes") break;
+
+                // add new line if conintuing for clean output
+                Console.WriteLine();
+            } while (true);
+            
+
         }
     }
 }
