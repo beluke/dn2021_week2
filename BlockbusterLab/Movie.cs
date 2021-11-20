ea007e88751fc937bbd1a30f9a5d9a8a3f9cc92b
Luke Belinc <ev2601@wayne.edu>
2021-11-20 03:05:49 -0500

    moved classes to separate files

---
+++ BlockbusterLab/Movie.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlockbusterLab
+{
+    abstract class Movie
+    {
+        public string Title { get; set; }
+        public Genre Category { get; set; }
+        public int RunTime { get; set; }
+        public string[] Scenes { get; set; }
+        public virtual void PrintInfo()
+        {
+            Console.Write(
+                "\nTitle: " + this.Title +
+                "\nCategory: " + this.GetCategory() +
+                "\nRuntime: " + this.RunTime + " minutes");
+        }
+
+        public void PrintScenes()
+        {
+            int i = 1;
+            foreach (var scene in this.Scenes)
+            {
+                Console.WriteLine($"\tScene {i++}: {scene}");
+            }
+        }
+
+        public string GetCategory()
+        {
+            switch (this.Category)
+            {
+                case Genre.Drama:
+                    return "Drama";
+                case Genre.Comedy:
+                    return "Comedy";
+                case Genre.Horror:
+                    return "Horror";
+                case Genre.Romance:
+                    return "Romance";
+                case Genre.Action:
+                    return "Action";
+                default:
+                    return "Default";
+            }
+        }
+    }
+}
---
+++ BlockbusterLab/VHS.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlockbusterLab
+{
+    class VHS : Movie
+    {
+        public VHS(string title, Genre genre, int runtime, string[] scenes)
+        {
+            Title = title;
+            Category = genre;
+            RunTime = runtime;
+            Scenes = scenes;
+            CurrentTime = 0;
+        }
+        public int CurrentTime { get; set; }
+
+        public override void PrintInfo()
+        {
+            Console.Write(
+                "\nTitle: " + this.Title +
+                "\nCategory: " + this.GetCategory() +
+                "\nRuntime: " + this.RunTime + " minutes\n");
+        }
+
+        public void Play()
+        {
+            if (CurrentTime >= Scenes.Length) Console.WriteLine("Movie has ended, please rewind...");
+            Console.WriteLine($"Current scene: {Scenes[CurrentTime++]}");
+        }
+
+        public void Rewind()
+        {
+            CurrentTime = 0;
+        }
+    }
+}
