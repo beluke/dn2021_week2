a9162bf8be7ff29d4472430b0c9bae379d0e8f8c
Luke Belinc <ev2601@wayne.edu>
2021-11-24 14:41:16 -0500

    changed method name for printing pointer values

--- PointersExample/Program.cs
+++ PointersExample/Program.cs
@@ -11,34 +11,34 @@ namespace PointersExample
             // update data at location p
             *p = 'p';
             // print pointer location
-            GetPointerValue(p);
+            PrintPointerValue(p);
             
             // make new char c
             char c = 'c';
             // print location of c using ref
-            GetPointerValue(ref c);
+            PrintPointerValue(ref c);
 
             // new pointer to location of c
             char* q = &c;
             // print location of c using ref to q
-            GetPointerValue(ref q);
+            PrintPointerValue(ref q);
         }
 
-        static unsafe void GetPointerValue(char* p)
+        static unsafe void PrintPointerValue(char* p)
         {
             // cast to IntPtr to print out actual address, otherwise it is not printable
             IntPtr addr = (IntPtr)p;
             Console.WriteLine("0x" + addr.ToString("x").ToUpper());
         }
 
-        static unsafe void GetPointerValue(ref char* p)
+        static unsafe void PrintPointerValue(ref char* p)
         {
             // cast p to IntPtr to print out actual address
             IntPtr addr = (IntPtr)p;
             Console.WriteLine("0x" + addr.ToString("x").ToUpper());
         }
 
-        static unsafe void GetPointerValue(ref char c)
+        static unsafe void PrintPointerValue(ref char c)
         {
             // need fixed specifier to take value of unfixed reference
             fixed (char *p = &c)
